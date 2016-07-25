b43968ff21242efef8a8121855e43a0c51c1838e
mario <mario@mate.bme.northwestern.edu>
2016-07-25 17:03:34 -0500

    working

--- main.f90
+++ main.f90
@@ -21,6 +21,7 @@ use bulk
 use seed1
 use longs
 use kai
+use string
 implicit none
 
 integer ncha
@@ -30,7 +31,7 @@ real*8 Na ! avogadros' number
 parameter (Na=6.02d23)
 integer cc,ccc
 
-real*8 x1(ntot),xg1(ntot)   ! density solvent iteration vector
+real*8 x1((ntot+1)*(NS-2)),xg1((ntot+1)*(NS-2))   ! density solvent iteration vector
 real*8 zc(ntot)           ! z-coordinate layer 
 
 integer n                 ! number of lattice sites
@@ -43,7 +44,6 @@ INTEGER temp
 real*8 tempr
 real*8 tmp
 
-REAL*8 xfile(ntot)                        
 real*8 algo, algo2                  
 
 real*8 chains(3,long,ncha_max) ! chains(x,i,l)= coordinate x of segement i ,x=2 y=3,z=1
@@ -84,66 +84,45 @@ vpol= ((4.0/3.0)*pi*(0.3)**3)/vsol  ! volume polymer segment in units of vsol
 xsolbulk = 1.0
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! read first and last
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+open(unit=20, file='first.dat')
+open(unit=21, file='last.dat')
+
+do i=1,n
+read(20,*)tempr,xfirst(i) 
+read(21,*)tempr,xlast(i) 
+enddo   
+
+close(20)
+close(21)
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! Solver
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!     init guess all 1.0 
+!  make initial guess by interpolation
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+do ii = 1, NS-2
 do i=1,n
-xg1(i)=1.0
-x1(i)=1.0
-zc(i)= (i-0.5) * delta
+xg1(i+(ii-1)*n) = (xlast(i)-xfirst(i))*float(ii)/float(NS-1) + xfirst(i)
+enddo
 enddo
 
-!     init guess from files fort.100 (solvent) and fort.200 (potential)                      
+!!! Initial guess for LM = 0
 
-if (infile.ge.1) then
-do i=1,n
-read(100,*)tempr,x1(i) 
-xg1(i) = x1(i)  ! solvent
-enddo   
-endif
+do i = 1, NS-2
+xg1(n*(NS-2)+ii) = 0.0
+enddo
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! CHAIN GENERATION
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+x1 = xg1
 
-print*, 'Calling RIS chain generator'
-in1n = 0
-
-call initcha              ! init matrices for chain generation
-conf=0                    ! counter of number of conformations
-
-newcuantas = 0
-
-do while (conf.lt.cuantas)
-call cadenas1(chains,ncha) ! generate only chains with first segment at z > 0
-
-do j=1,ncha
-  if(conf.lt.cuantas) then
-   conf=conf+1
-
-   flag = 0
-      do k=1,long
-        temp=int(chains(1,k,j)/delta)+1  ! put segments into the correct layer
-           if(temp.gt.ntot)flag = 1
-      enddo ! k
-
-      if(flag.eq.0) then
-      newcuantas=newcuantas+1
-       do k=1,long
-        temp=int(chains(1,k,j)/delta)+1  ! put segments into the correct layer
-        in1n(newcuantas,temp) =  in1n(newcuantas,temp) + 1
-       enddo ! k
-      endif
-   endif
-enddo ! j
-enddo ! while
-
-print*,"Chains ready"
-
+do i = n
+zc(i)= (i-0.5) * delta
+enddo
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !     computation starts
